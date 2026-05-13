80ffa7e01cfb7801c808a30d98a1d77c8dfc6219
Trax Bagley <kf7mxe@gmail.com>
2026-05-12 23:39:20 -0600

    debug isDemo check

--- apps/src/jsMain/kotlin/com/kf7mxe/inglenook/demo/demoDetection.js.kt
+++ apps/src/jsMain/kotlin/com/kf7mxe/inglenook/demo/demoDetection.js.kt
@@ -4,5 +4,8 @@ import kotlinx.browser.window
 
 actual fun isDemoWebsite(): Boolean {
     val hostname = window.location.hostname
+    println("DEBUG window.location.hostname: ${window.location.hostname}")
+    println("DEBUG HOSTNAME: $hostname")
+    println("DEBUG hostname.contains(\"https://kf7mxe.github.io/inglenook ${hostname.contains("https://kf7mxe.github.io/inglenook")}")
     return hostname.contains("https://kf7mxe.github.io/inglenook/")
 }
